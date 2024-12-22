0a15c3c9254fb69efa212111646bf8798854b68b
Devinzhang91 <devinzhang91@gmail.com>
2024-12-22 11:21:44 +0800

    add target [esplay-nano], add USB msc.

--- components/retro-go/targets/esplay-nano/config.h
+++ components/retro-go/targets/esplay-nano/config.h
@@ -1,7 +1,7 @@
 // REF: https://wiki.odroid.com/odroid_go/odroid_go
 
 // Target definition
-#define RG_TARGET_NAME             "ESPPLAY-NANO"
+#define RG_TARGET_NAME             "ESPLAY-NANO"
 
 // Storage
 #define RG_STORAGE_ROOT             "/sd"
@@ -18,7 +18,7 @@
 #define RG_AUDIO_USE_EXT_DAC        1   // 0 = Disable, 1 = Enable
 
 // Video
-#define RG_SCREEN_DRIVER            0   // 0 = ILI9341 but also works on ST7789 with below RG_SCREEN_INIT()
+#define RG_SCREEN_DRIVER            1   // 0 = ILI9341  1 = ST7789
 #define RG_SCREEN_HOST              SPI2_HOST
 #define RG_SCREEN_SPEED             SPI_MASTER_FREQ_40M // SPI_MASTER_FREQ_80M
 #define RG_SCREEN_BACKLIGHT         1
@@ -30,38 +30,42 @@
 #define RG_SCREEN_MARGIN_LEFT       0
 #define RG_SCREEN_MARGIN_RIGHT      0
 
+
+
 #define ST7789_MADCTL 0x36 // Memory Access Control
+#define ST7789_MADCTL_MY 0x80
+#define ST7789_MADCTL_MV 0x40
 #define ST7789_MADCTL_MV 0x20
+#define ST7789_MADCTL_ML 0x10
 #define ST7789_MADCTL_RGB 0x00
 #define ST7789_MADCTL_BGR 0x08
 
 #define RG_SCREEN_INIT()                                                                                         \
-    ILI9341_CMD(0xCF, 0x00, 0xc3, 0x30);                                                                         \
-    ILI9341_CMD(0xED, 0x64, 0x03, 0x12, 0x81);                                                                   \
-    ILI9341_CMD(0xE8, 0x85, 0x00, 0x78);                                                                         \
-    ILI9341_CMD(0xCB, 0x39, 0x2c, 0x00, 0x34, 0x02);                                                             \
-    ILI9341_CMD(0xF7, 0x20);                                                                                     \
-    ILI9341_CMD(0xEA, 0x00, 0x00);                                                                               \
-    ILI9341_CMD(0xC0, 0x1B);                 /* Power control   //VRH[5:0] */                                    \
-    ILI9341_CMD(0xC1, 0x12);                 /* Power control   //SAP[2:0];BT[3:0] */                            \
-    ILI9341_CMD(0xC5, 0x32, 0x3C);           /* VCM control */                                                   \
-    ILI9341_CMD(0xC7, 0x91);                 /* VCM control2 */                                                  \
-    ILI9341_CMD(ST7789_MADCTL, (ST7789_MADCTL_MV | ST7789_MADCTL_BGR));                                          \
-    ILI9341_CMD(0xB1, 0x00, 0x10);           /* Frame Rate Control (1B=70, 1F=61, 10=119) */                     \
-    ILI9341_CMD(0xB6, 0x0A, 0xA2);           /* Display Function Control */                                      \
-    ILI9341_CMD(0xF6, 0x01, 0x30);                                                                               \
-    ILI9341_CMD(0xF2, 0x00); /* 3Gamma Function Disable */                                                       \
-    ILI9341_CMD(0xE0, 0xD0, 0x00, 0x05, 0x0E, 0x15, 0x0D, 0x37, 0x43, 0x47, 0x09, 0x15, 0x12, 0x16, 0x19);       \
-    ILI9341_CMD(0xE1, 0xD0, 0x00, 0x05, 0x0D, 0x0C, 0x06, 0x2D, 0x44, 0x40, 0x0E, 0x1C, 0x18, 0x16, 0x19);
-
+    ST7789_CMD(0xCF, 0x00, 0xc3, 0x30);                                                                         \
+    ST7789_CMD(0xED, 0x64, 0x03, 0x12, 0x81);                                                                   \
+    ST7789_CMD(0xE8, 0x85, 0x00, 0x78);                                                                         \
+    ST7789_CMD(0xCB, 0x39, 0x2c, 0x00, 0x34, 0x02);                                                             \
+    ST7789_CMD(0xF7, 0x20);                                                                                     \
+    ST7789_CMD(0xEA, 0x00, 0x00);                                                                               \
+    ST7789_CMD(0xC0, 0x1B);                 /* Power control   //VRH[5:0] */                                    \
+    ST7789_CMD(0xC1, 0x12);                 /* Power control   //SAP[2:0];BT[3:0] */                            \
+    ST7789_CMD(0xC5, 0x32, 0x3C);           /* VCM control */                                                   \
+    ST7789_CMD(0xC7, 0x91);                 /* VCM control2 */                                                  \
+    ST7789_CMD(ST7789_MADCTL, (ST7789_MADCTL_MY | ST7789_MADCTL_MV | ST7789_MADCTL_BGR));                                          \
+    ST7789_CMD(0xB1, 0x00, 0x10);           /* Frame Rate Control (1B=70, 1F=61, 10=119) */                     \
+    ST7789_CMD(0xB6, 0x0A, 0xA2);           /* Display Function Control */                                      \
+    ST7789_CMD(0xF6, 0x01, 0x30);                                                                               \
+    ST7789_CMD(0xF2, 0x00); /* 3Gamma Function Disable */                                                       \
+    ST7789_CMD(0xE0, 0xD0, 0x00, 0x05, 0x0E, 0x15, 0x0D, 0x37, 0x43, 0x47, 0x09, 0x15, 0x12, 0x16, 0x19);       \
+    ST7789_CMD(0xE1, 0xD0, 0x00, 0x05, 0x0D, 0x0C, 0x06, 0x2D, 0x44, 0x40, 0x0E, 0x1C, 0x18, 0x16, 0x19);
 
 // Input
 // Refer to rg_input.h to see all available RG_KEY_* and RG_GAMEPAD_*_MAP types
 
 #define RG_GAMEPAD_GPIO_MAP {\
-    {RG_KEY_UP,     GPIO_NUM_7,  GPIO_PULLUP_ONLY, 0},\
-    {RG_KEY_RIGHT,  GPIO_NUM_6,  GPIO_PULLUP_ONLY, 0},\
-    {RG_KEY_DOWN,   GPIO_NUM_46, GPIO_PULLUP_ONLY, 0},\
+    {RG_KEY_UP,     GPIO_NUM_46,  GPIO_PULLUP_ONLY, 0},\
+    {RG_KEY_RIGHT,  GPIO_NUM_7,  GPIO_PULLUP_ONLY, 0},\
+    {RG_KEY_DOWN,   GPIO_NUM_6, GPIO_PULLUP_ONLY, 0},\
     {RG_KEY_LEFT,   GPIO_NUM_45, GPIO_PULLUP_ONLY, 0},\
     {RG_KEY_SELECT, GPIO_NUM_16, GPIO_PULLUP_ONLY, 0},\
     {RG_KEY_START,  GPIO_NUM_17, GPIO_PULLUP_ONLY, 0},\
@@ -87,7 +91,7 @@
 #define RG_GPIO_LCD_CLK             GPIO_NUM_48
 #define RG_GPIO_LCD_CS              GPIO_NUM_NC
 #define RG_GPIO_LCD_DC              GPIO_NUM_47
-#define RG_GPIO_LCD_BCKL            GPIO_NUM_NC
+#define RG_GPIO_LCD_BCKL            GPIO_NUM_39
 #define RG_GPIO_LCD_RST             GPIO_NUM_3
 
 #define RG_GPIO_SDSPI_MISO          GPIO_NUM_9
@@ -100,3 +104,4 @@
 #define RG_GPIO_SND_I2S_WS          42
 #define RG_GPIO_SND_I2S_DATA        40
 // #define RG_GPIO_SND_AMP_ENABLE      18
+
