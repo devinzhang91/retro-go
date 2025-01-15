100fbcb33c97bed9f5a0c0302a8c21949768bdb9
Devinzhang91 <devinzhang91@gmail.com>
2025-01-15 23:09:18 +0800

    modified SCH

--- components/retro-go/targets/esplay-nano/config.h
+++ components/retro-go/targets/esplay-nano/config.h
@@ -1,15 +1,14 @@
 // REF: https://wiki.odroid.com/odroid_go/odroid_go
 
 // Target definition
-
 #define RG_TARGET_NAME             "ESPLAY-NANO"
 
 // Storage
 #define RG_STORAGE_ROOT             "/sd"
-#define RG_STORAGE_SDSPI_HOST       SPI3_HOST
-#define RG_STORAGE_SDSPI_SPEED      SDMMC_FREQ_DEFAULT
-// #define RG_STORAGE_SDMMC_HOST       SDMMC_HOST_SLOT_1
-// #define RG_STORAGE_SDMMC_SPEED      SDMMC_FREQ_DEFAULT
+// #define RG_STORAGE_SDSPI_HOST       SPI3_HOST
+// #define RG_STORAGE_SDSPI_SPEED      SDMMC_FREQ_DEFAULT
+#define RG_STORAGE_SDMMC_HOST       SDMMC_HOST_SLOT_1
+#define RG_STORAGE_SDMMC_SPEED      SDMMC_FREQ_DEFAULT
 // #define RG_STORAGE_FLASH_PARTITION  "vfs"
 
 #define RG_USB_MSC                  1   // 0 = Disable, 1 = Enable, Only for esp32s3
@@ -64,15 +63,15 @@
 // Refer to rg_input.h to see all available RG_KEY_* and RG_GAMEPAD_*_MAP types
 
 #define RG_GAMEPAD_GPIO_MAP {\
-    {RG_KEY_UP,     GPIO_NUM_46,  GPIO_PULLUP_ONLY, 0},\
-    {RG_KEY_RIGHT,  GPIO_NUM_7,  GPIO_PULLUP_ONLY, 0},\
-    {RG_KEY_DOWN,   GPIO_NUM_6, GPIO_PULLUP_ONLY, 0},\
-    {RG_KEY_LEFT,   GPIO_NUM_45, GPIO_PULLUP_ONLY, 0},\
+    {RG_KEY_UP,     GPIO_NUM_8,  GPIO_PULLUP_ONLY, 0},\
+    {RG_KEY_RIGHT,  GPIO_NUM_18,  GPIO_PULLUP_ONLY, 0},\
+    {RG_KEY_DOWN,   GPIO_NUM_3, GPIO_PULLUP_ONLY, 0},\
+    {RG_KEY_LEFT,   GPIO_NUM_46, GPIO_PULLUP_ONLY, 0},\
     {RG_KEY_SELECT, GPIO_NUM_16, GPIO_PULLUP_ONLY, 0},\
     {RG_KEY_START,  GPIO_NUM_17, GPIO_PULLUP_ONLY, 0},\
-    {RG_KEY_MENU,   GPIO_NUM_18, GPIO_PULLUP_ONLY, 0},\
-    {RG_KEY_OPTION, GPIO_NUM_8,  GPIO_PULLUP_ONLY, 0},\
-    {RG_KEY_A,      GPIO_NUM_15, GPIO_PULLUP_ONLY, 0},\
+    {RG_KEY_MENU,   GPIO_NUM_6, GPIO_PULLUP_ONLY, 0},\
+    {RG_KEY_OPTION, GPIO_NUM_15,  GPIO_PULLUP_ONLY, 0},\
+    {RG_KEY_A,      GPIO_NUM_7, GPIO_PULLUP_ONLY, 0},\
     {RG_KEY_B,      GPIO_NUM_5,  GPIO_PULLUP_ONLY, 0},\
 }
 
@@ -84,7 +83,7 @@
 #define RG_BATTERY_CALC_VOLTAGE(raw) ((raw) * 2.f * 0.001f)
 
 // Status LED
-#define RG_GPIO_LED                 GPIO_NUM_38
+#define RG_GPIO_LED                 GPIO_NUM_45
 
 // SPI Display (back up working)
 #define RG_GPIO_LCD_MISO            GPIO_NUM_NC
@@ -93,16 +92,23 @@
 #define RG_GPIO_LCD_CS              GPIO_NUM_NC
 #define RG_GPIO_LCD_DC              GPIO_NUM_47
 #define RG_GPIO_LCD_BCKL            GPIO_NUM_39
-#define RG_GPIO_LCD_RST             GPIO_NUM_3
+#define RG_GPIO_LCD_RST             GPIO_NUM_38
 
-#define RG_GPIO_SDSPI_MISO          GPIO_NUM_9
-#define RG_GPIO_SDSPI_MOSI          GPIO_NUM_11
-#define RG_GPIO_SDSPI_CLK           GPIO_NUM_13
-#define RG_GPIO_SDSPI_CS            GPIO_NUM_10
+#ifdef RG_STORAGE_SDSPI_HOST
+    #define RG_GPIO_SDSPI_MISO          GPIO_NUM_9
+    #define RG_GPIO_SDSPI_MOSI          GPIO_NUM_11
+    #define RG_GPIO_SDSPI_CLK           GPIO_NUM_13
+    #define RG_GPIO_SDSPI_CS            GPIO_NUM_10
+#endif
+#ifdef RG_STORAGE_SDMMC_HOST
+    #define RG_GPIO_SDSPI_D0           GPIO_NUM_9
+    #define RG_GPIO_SDSPI_CMD          GPIO_NUM_11
+    #define RG_GPIO_SDSPI_CLK          GPIO_NUM_13
+    #define RG_GPIO_SDSPI_CS           GPIO_NUM_10
+#endif
 
 // External I2S DAC
 #define RG_GPIO_SND_I2S_BCK         41
 #define RG_GPIO_SND_I2S_WS          42
 #define RG_GPIO_SND_I2S_DATA        40
 // #define RG_GPIO_SND_AMP_ENABLE      18
-
